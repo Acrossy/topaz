0e8a3777eb284053b5088d8c7fadc3342d9f6a02
Across <55287958+Acrossy@users.noreply.github.com>
2020-10-11 16:46:37 +0100

    Update beast_roll.lua
    The Beast roll is not adding Ranged ATK to pets including PUP bots so I am trying to submit this fix by adding the lines required for the roll to behave like it should.
    Please see if it's in accordance.
    Thank you.

--- scripts/globals/effects/beast_roll.lua
+++ scripts/globals/effects/beast_roll.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addPetMod(tpz.mod.ATTP, effect:getPower())
+    target:addMod(tpz.mod.RATTP, effect:getPower())
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +16,5 @@ end
 
 function onEffectLose(target, effect)
     target:delPetMod(tpz.mod.ATTP, effect:getPower())
+    target:delMod(tpz.mod.RATTP, effect:getPower())
 end
